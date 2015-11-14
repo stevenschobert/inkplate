ecc708cf7a2e6a5535fa1fe0de02f523782c0b38
Steven Schobert <spschobert@gmail.com>
2015-11-14 00:08:36 -0600

    more post serialization

--- app/api/xmlrpc/meta_weblog.rb
+++ app/api/xmlrpc/meta_weblog.rb
@@ -2,24 +2,80 @@
 # https://codex.wordpress.org/XML-RPC_MetaWeblog_API
 module Api
   class MetaWeblog
+    STATUSES = {
+      "published"   => "publish",     # A published post or page
+      "pending"     => "pending",     # post is pending review
+      "draft"       => "draft",       # a post in draft status
+      "auto-draft"  => "auto-draft",  # a newly created post, with no content
+      "future"      => "future",      # a post to publish in the future
+      "private"     => "private",     # not visible to users who are not logged in
+      "inherit"     => "iherit",      # a revision. see get_children.
+      "trashed"     => "trashed"      # post is in trashbin. added with Version 2.9.
+    }.freeze
 
     include XmlRpc
 
     self.namespace = "metaWeblog"
 
+    def getPost(post_id, username, password)
+      if post = Post.where(id: post_id).first
+        serialize_post(post)
+      else
+        raise "not found"
+      end
+    end
+
     def getRecentPosts(blog_id, username, password, max_posts = 10)
       posts = Post.limit(max_posts).order(created_at: :desc)
 
-      posts.map do |post|
-        {
-          postid: post.id,
-          title: post.title,
-          dateCreated: post.created_at.localtime.to_datetime,
-          date_created_gmt: post.created_at.utc,
-          date_modified: post.updated_at.localtime.to_datetime,
-          date_modified_gmt: post.updated_at.utc
-        }
+      posts.map{ |post| serialize_post(post) }
+    end
+
+    def newPost(blog_id, username, password, content, publish)
+      post = Post.new({
+        title: content["title"]
+      })
+
+      if post.save
+        post.id.to_s
+      else
+        raise "Should return error here"
       end
+    end
+
+    protected
+
+    def serialize_post(post)
+      categories = Category
+        .select("c.name")
+        .from("categories c, categorized_posts cp")
+        .where("cp.post_id = ?", [post.id])
+
+      {
+        postid: post.id,
+        title: post.title.to_s,
+        dateCreated: post.created_at.localtime.to_datetime,
+        date_created_gmt: post.created_at.utc,
+        date_modified: post.updated_at.localtime.to_datetime,
+        date_modified_gmt: post.updated_at.utc,
+        wp_post_thumbnail: "",
+        permaLink: "http://google.com",
+        categories: categories.map{ |c| c.name.to_s },
+        mt_keywords: "",
+        mt_excerpt: "",
+        mt_text_more: "",
+        wp_more_text: "",
+        mt_allow_comments: 0,
+        mt_allow_pings: 0,
+        wp_slug: "testslug",
+        wp_password: "",
+        wp_author_id: "",
+        wp_author_display_name: "",
+        post_status: STATUSES["published"],
+        wp_post_format: "",
+        sticky: false,
+        custom_fields: []
+      }
     end
 
   end
