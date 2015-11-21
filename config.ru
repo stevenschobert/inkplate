5c796cf8d451349b154b29fabebfe3292777080b
Steven Schobert <spschobert@gmail.com>
2015-11-21 11:25:02 -0600

    setup error logging

--- config.ru
+++ config.ru
@@ -5,6 +5,11 @@ require 'rack/xmlrpc_server'
 
 server = Proc.new { |env| [200, {}, []] }
 
+logger = Logger.new('log/requests.log')
+
+use Rack::CommonLogger, logger
+use Rack::Logger
+
 use JsonServer
 use XmlRpcServer
 
--- lib/rack/xmlrpc_server.rb
+++ lib/rack/xmlrpc_server.rb
@@ -14,32 +14,59 @@ class XmlRpcServer < XMLRPC::BasicServer
     setup!
   end
 
-  def setup!
-    Api::XmlRpc.implementations.each do |impl|
-      self.add_handler(XMLRPC.iPIMethods(impl[:namespace]), impl[:klass].new)
-    end
-  end
-
   def call(env)
     status, headers, body = app.call(env)
     req = Rack::Request.new(env)
 
     if req.content_type == MATCH_TYPE
       data = req.body.read
+      method, params = parser().parseMethodCall(data)
 
-      # TODO: implement logging
-      puts data
+      method_response = begin
+        [true, dispatch(method, *params)]
+      rescue XMLRPC::FaultException => e
+        log_request_failure({
+          logger: env["rack.logger"],
+          request: req,
+          body: data,
+          exception: e
+        })
 
-      reply = process(data)
+        [false, e]
+      rescue Exception => e
+        log_request_failure({
+          logger: env["rack.logger"],
+          request: req,
+          body: data,
+          exception: e
+        })
 
-      # TODO: implement logging
-      puts reply
+        [false, XMLRPC::FaultException.new(XMLRPC::BasicServer::ERR_UNCAUGHT_EXCEPTION, "Uncaught exception #{e.message} in method #{method}")]
+      end
+
+      reply = create().methodResponse(*method_response)
 
       [200, { "Content-Type" => "text/xml" }, [reply]]
     else
       [status, headers, body]
     end
-
   end
+
+  def log_request_failure(logger: nil, request: nil, body: nil, exception: nil)
+    if logger
+      if exception
+        logger.error("XMLRPC Error: #{ exception.message }:\n #{ body }")
+      else
+        logger.error("Uknown Error for XMLRPC request:\n #{ body }")
+      end
+    end
+  end
+
+  def setup!
+    Api::XmlRpc.implementations.each do |impl|
+      self.add_handler(XMLRPC.iPIMethods(impl[:namespace]), impl[:klass].new)
+    end
+  end
+
 end
 
