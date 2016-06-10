a7435d1a970156bd01173717fc336f34cbf3cdc3
Steven Schobert <spschobert@gmail.com>
2016-06-10 01:22:09 -0500

    avoid naming conflicts in uploads

--- app/api/xmlrpc/meta_weblog.rb
+++ app/api/xmlrpc/meta_weblog.rb
@@ -66,7 +66,7 @@ module Api
         :add
       end
 
-      create_response = dropbox.upload(data, path: path, write_mode: mode)
+      create_response = dropbox.upload(data, path: path, write_mode: mode, autorename: true)
       link_response = if create_response
         dropbox.create_link(path: create_response["path_lower"])
       end
