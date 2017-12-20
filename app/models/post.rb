40862db459b410aae1dfab25bc1e87e3a7860877
Steven Schobert <spschobert@gmail.com>
2017-12-20 00:38:02 -0600

    add micro format for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
 
-  validates_presence_of :slug
+  validates_presence_of :slug, unless: -> { micro? }
 
   has_many :categorized_posts, dependent: :destroy
 
   enum status: [ :publish, :pending, :draft, :invisible ]
-  enum kind: [ :post, :page ]
+  enum kind: [ :post, :page, :micro ]
 
 end
