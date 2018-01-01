7ab537a0a3c5d67d14d9ad3ea39bbd63506bf4dd
Steven Schobert <spschobert@gmail.com>
2018-01-01 10:06:17 -0600

    add hooks for slugs and titles depending on type

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,19 @@ class Post < ActiveRecord::Base
   enum status: [ :publish, :pending, :draft, :invisible ]
   enum kind: [ :post, :page, :micro ]
 
+  before_validation :slugify, unless: :micro?
+  before_validation :strip_title, if: :micro?
+
+  protected
+
+  def strip_title
+    self.title = nil
+  end
+
+  def slugify
+    if slug.to_s.empty?
+      self.slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
+  end
+
 end
